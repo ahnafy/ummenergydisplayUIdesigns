776e51063deaa1bb3bff0201adac387ae6fbf6fe
ahnafy <yasin011@morris.umn.edu>
2018-03-19 17:08:02 -0600

    Gui mockup intialized

---
+++ WebTemplate/js/config.js
@@ -0,0 +1,1 @@
+window.config={"logo":"","background":"#C4C4C4","shell":"desktop","showControlPanel":false,"iOSGuide":false,"alwaysShowLinks":false,"hoverShowLinks":false,"hoverShowRemarks":true,"showComments":true,"showQRCode":false,"hintLinks":true,"scrollRemarks":true,"isShowAllPageNode":false,"isShowPreviewTools":false,"showRemarkFlag":false}
